206df3b051caf04ece39814c174fb8c4b8a7b5bf
Pablo Reszczynski <pablore@me.com>
2017-09-13 18:27:37 -0300

    added localhost to domains

--- tasklist/settings.py
+++ tasklist/settings.py
@@ -27,7 +27,13 @@ SECRET_KEY = 't$lxqy6hlp^e@o637fp8+id%15f+xxw_1#6ls-#vfupuw8nj+o'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [u'tasklist-sleiva.c9users.io', 'agila2.herokuapp.com']
+ALLOWED_HOSTS = [
+    u'tasklist-sleiva.c9users.io',
+    'agila2.herokuapp.com',
+    'localhost',
+    '127.0.0.1'
+]
+
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 
